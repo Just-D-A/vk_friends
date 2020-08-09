6fb761b8c8ebed4154b6426470242d5154387357
Just-D-A <prospeedcuber@gmail.com>
2020-08-09 17:52:54 +0300

    add login presenter logic

--- app/src/main/java/com/example/vk_friends/activities/LoginActivity.kt
+++ app/src/main/java/com/example/vk_friends/activities/LoginActivity.kt
@@ -1,30 +1,56 @@
 package com.example.vk_friends.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.arellomobile.mvp.MvpAppCompatActivity
-
+import com.arellomobile.mvp.presenter.InjectPresenter
 import com.example.vk_friends.R
+import com.example.vk_friends.presenters.LoginPresenter
 import com.example.vk_friends.views.LoginView
+import com.github.rahatarmanahmed.cpv.CircularProgressView
 
 class LoginActivity : MvpAppCompatActivity(), LoginView {
+
+
+    private lateinit var mTxtHello: TextView
+    private lateinit var mBtnEnter: Button
+    private lateinit var mCpvWait: CircularProgressView
+
+    @InjectPresenter
+    lateinit var loginPresenter: LoginPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mTxtHello = findViewById(R.id.txt_login_hello)
+        mBtnEnter = findViewById(R.id.button)
+        mCpvWait = findViewById(R.id.cpv_login)
+
+        mBtnEnter.setOnClickListener {
+            loginPresenter.login(isSuccess = true)
+        }
     }
 
     override fun startLoading() {
-        TODO("Not yet implemented")
+        mBtnEnter.visibility = View.GONE
+        mCpvWait.visibility = View.VISIBLE
     }
 
     override fun endLoading() {
-        TODO("Not yet implemented")
+        mBtnEnter.visibility = View.VISIBLE
+        mCpvWait.visibility = View.GONE
     }
 
     override fun openFriends() {
-        TODO("Not yet implemented")
+        startActivity(Intent(applicationContext, FriendsActivity::class.java))
     }
 
     override fun showError(text: String) {
-        TODO("Not yet implemented")
+        Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/vk_friends/presenters/LoginPresenter.kt
+++ app/src/main/java/com/example/vk_friends/presenters/LoginPresenter.kt
@@ -1,9 +1,21 @@
 package com.example.vk_friends.presenters
 
+import android.os.Handler
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import com.example.vk_friends.views.LoginView
 
 @InjectViewState
 class LoginPresenter: MvpPresenter<LoginView>() {
+    fun login(isSuccess: Boolean) {
+        viewState.startLoading()
+        Handler().postDelayed({
+            viewState.endLoading()
+            if(isSuccess) {
+                viewState.openFriends()
+            } else {
+                viewState.showError(text = "Login data is incorrect")
+            }
+        }, 500)
+    }
 }
